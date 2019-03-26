087f79ddaab3a180e1ff425c2d307e60c5775dca
yanbin <342013708@qq.com>
2019-03-26 11:37:52 +0800

    二次曲线线性拟合

--- multiLinear/muti_linear_05.py
+++ multiLinear/muti_linear_05.py
@@ -1,31 +1,29 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 #定义参数
-w_target = np.array([0.5, 3, 2.4])
+w_target = np.array([0.5, 3])
 b_target = np.array([0.9])
 
-f_des = 'y = {:.2f} + {:.2f} * x + {:.2f} * x^2 +{:.2f} * x^3'.format(
-    b_target[0], w_target[0], w_target[1], w_target[2]
+f_des = 'y = {:.2f} + {:.2f} * x + {:.2f} * x^2'.format(
+    b_target[0], w_target[0], w_target[1]
 )
 print(f_des)
 
 x_sample = np.arange(-3,3.1,0.1)
-y_sample = b_target[0] + w_target[0]*x_sample + w_target[1]*x_sample**2 + w_target[2]*x_sample**3
+y_sample = b_target[0] + w_target[0]*x_sample + w_target[1]*x_sample**2
 
-x_train = np.stack([x_sample**i for i in range(1,4)], axis=1)
+
+x_train = np.stack([x_sample**i for i in range(1,3)], axis=1)
 x_train = tf.constant(x_train, dtype=tf.float32, name='x_train')
 y_train = tf.constant(y_sample, dtype=tf.float32, name='y_train')
 
-w = tf.Variable(initial_value=tf.random_normal(shape=(3,1)), dtype=tf.float32, name='weights')
+w = tf.Variable(initial_value=tf.random_normal(shape=(2,1)), dtype=tf.float32, name='weights')
 b = tf.Variable(initial_value=0, dtype=tf.float32, name='bias')
 
 def multi_linear(x):
@@ -41,6 +39,7 @@ loss = tf.reduce_mean(tf.square(y_train-y_))
 loss_numpy = sess.run(loss)
 print(loss_numpy)
 
+
 w_grad,b_grad = tf.gradients(loss,[w,b])
 
 print(w_grad.eval(session=sess))
@@ -53,10 +52,24 @@ b_update = b.assign_sub(lr*b_grad)
 
 sess.run([w_update,b_update])
 
+
+
+
+
 x_train_value = x_train.eval(session=sess)
 y_train_value = y_train.eval(session=sess)
 y_pred_value = y_.eval(session=sess)
 loss_numpy=loss.eval(session=sess)
+
+for e in range(2000):
+    sess.run([w_update,b_update])
+    x_train_value = x_train.eval(session=sess)
+    y_train_value = y_train.eval(session=sess)
+    y_pred_value = y_.eval(session=sess)
+    loss_numpy=loss.eval(session=sess)
+
+    if(e+1)%20==0:
+        print('epoch: {}, loss: {}'.format(e+1, loss_numpy))
 
 
 plt.plot(x_train_value[:,0], y_pred_value, label='fitting curve', color='r')
@@ -65,10 +78,6 @@ plt.legend()
 plt.title('loss: %.4f' % loss_numpy)
 sess.close()
 plt.show()
-
-
-
-
 
 
 
