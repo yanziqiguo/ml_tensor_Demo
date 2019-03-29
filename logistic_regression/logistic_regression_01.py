6c5dbe054bf76ffe92d94ac231c735655472c776
yanbin <342013708@qq.com>
2019-03-29 14:35:46 +0800

    逻辑回归

---
+++ logistic_regression/logistic_regression_01.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+# read data from file
+with open('./logistic_regression.txt', 'r') as f:
+    data_list = [i.split('\n')[0].split(',') for i in f.readlines()]
+    data = [(float(i[0]), float(i[1]), float(i[2])) for i in data_list]
+
+# 标准化
+x0_max = max([i[0] for i in data])
+x1_max = max([i[1] for i in data])
+data = [(i[0]/x0_max, i[1]/x1_max, i[2]) for i in data]
+
+x0 = list(filter(lambda  x:x[-1] == 0.0, data))
+x1 = list(filter(lambda  x:x[-1] == 1.0, data))
+
+plot_x0 = [i[0] for i in x0]
+plot_y0 = [i[1] for i in x0]
+plot_x1 = [i[0] for i in x1]
+plot_y1 = [i[1] for i in x1]
+
+plt.plot(plot_x0,plot_y0, 'ro', label='x_0')
+plt.plot(plot_x1, plot_y1, 'bo', label='x_1')
+plt.legend(loc='best')
+plt.title('logistic')
+plt.show()
+
+
+
+
+
+
